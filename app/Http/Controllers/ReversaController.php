83e98234a35adf5afaaec963970bbbb212f322ba
Rafael Belchior <rafaelb89.rb@gmail.com>
2024-01-30 12:11:17 -0300

    fix: refatorando rotas

---
+++ app/Http/Controllers/ReversaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ReversaController extends Controller
+{
+  public function index()
+  {
+    // $envios = DB::table('coletas')->join('envios','coletas.id','=','envios.coleta_id')->select('coletas.id',DB::raw('Count(envios.id) as qte'),DB::raw('sum(envios.valor_total) as total'),DB::raw('sum(envios.valor_desconto) as desconto'),'coletas.type')->where("coletas.user_id","=",5)->groupBy("coletas.id")->paginate();
+    return view('layouts.reversa.reversa');
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\Admin\{ReplySupportController, SupportController};
-use App\Http\Controllers\ColetaController;
-use App\Http\Controllers\EtiquetaController;
+use App\Http\Controllers\ColetasController;
+use App\Http\Controllers\EtiquetasController;
+use App\Http\Controllers\GerarEnvioController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReversaController;
 use App\Http\Controllers\Site\SiteController;
-use App\Models\Envio;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [SiteController::class, 'index']);
@@ -15,48 +14,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/etiquetas', function () {
-    $envios = DB::table('coletas')->join('envios','coletas.id','=','envios.coleta_id')->select('coletas.id',DB::raw('Count(envios.id) as qte'),DB::raw('sum(envios.valor_total) as total'),DB::raw('sum(envios.valor_desconto) as desconto'),'coletas.type')->where("coletas.user_id","=",5)->groupBy("coletas.id")->paginate();
-    return view('layouts.etiquetas',compact("envios"));
-})->middleware(['auth', 'verified'])->name('etiquetas');
+Route::middleware(['auth', 'verified'])->group(function () {
+  /*
+  Gerar envios
+  */
+  Route::get('/gerar', [GerarEnvioController::class, 'index'])->name('gerar');
 
-Route::get('/gerar', function () {
-    $envios = DB::table('coletas')->join('envios','coletas.id','=','envios.coleta_id')->select('coletas.id',DB::raw('Count(envios.id) as qte'),DB::raw('sum(envios.valor_total) as total'),DB::raw('sum(envios.valor_desconto) as desconto'),'coletas.type')->where("coletas.user_id","=",5)->groupBy("coletas.id")->paginate();
-    return view('layouts.gerar.gerar',compact("envios"));
-})->middleware(['auth', 'verified'])->name('gerar');
+  /*
+  Etiquetas
+  */
+  Route::get('/etiquetas', [EtiquetasController::class, 'index'])->name('etiquetas');
+  Route::get('/etiquetas/{id}', [EtiquetasController::class, 'show'])->name('etiqueta.show');
+  Route::get('/teste', [EtiquetasController::class, 'teste']);
 
-Route::get('/reversa', function () {
-    // $envios = DB::table('coletas')->join('envios','coletas.id','=','envios.coleta_id')->select('coletas.id',DB::raw('Count(envios.id) as qte'),DB::raw('sum(envios.valor_total) as total'),DB::raw('sum(envios.valor_desconto) as desconto'),'coletas.type')->where("coletas.user_id","=",5)->groupBy("coletas.id")->paginate();
-    return view('layouts.reversa.reversa');
-})->middleware(['auth', 'verified'])->name('reversa');
+  /*
+  Coletas
+  */
+  Route::get('coleta/{id}',[ColetasController::class, 'getlistItens'])->name('coleta.show');
 
-Route::get('/declaracoes', function () {
-    $envios = DB::table('coletas')->join('envios','coletas.id','=','envios.coleta_id')->select('coletas.id',DB::raw('Count(envios.id) as qte'),DB::raw('sum(envios.valor_total) as total'),DB::raw('sum(envios.valor_desconto) as desconto'),'coletas.type')->where("coletas.user_id","=",5)->groupBy("coletas.id")->paginate();
-    return view('layouts.declaracoes.declaracoes',compact("envios"));
-})->middleware(['auth', 'verified'])->name('declaracoes');
+  /*
+  Reversa
+  */
+  Route::get('reversa',[ReversaController::class, 'index'])->name('reversa');
 
-Route::get('/etiquetas/{id}', [EtiquetaController::class, 'show'])->middleware(['auth', 'verified'])->name('etiqueta.show');
-
-Route::get('/teste', [EtiquetaController::class, 'teste']);
-
-Route::get('coleta/{id}',[ColetaController::class, "getlistItens"])->middleware(['auth', 'verified'])->name('coleta.show');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::post('/supports/{id}/replies', [ReplySupportController::class, 'store'])->name('replies.store');
-    Route::delete('/supports/{id}/replies/{reply}', [ReplySupportController::class, 'destroy'])->name('replies.destroy');
-    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
-
-    // Route::resource('/supports', SupportController::class);
-    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+  /*
+  Declaracoes
+  */
+  Route::get('declaracoes',[ReversaController::class, 'index'])->name('declaracoes');
+  
+  /*
+  Profile
+  */
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__ . '/auth.php';
