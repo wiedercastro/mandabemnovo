1bd197037d5384866805b303e960e0f6e86257f1
Rafael Belchior <rafaelb89.rb@gmail.com>
2024-03-15 14:59:49 -0300

    att

--- public/js/etiquetas/busca_status_postagem.js
+++ public/js/etiquetas/busca_status_postagem.js
@@ -2,10 +2,13 @@ const buscaPeloStatusDaPostagem = () => {
   alert("OPA")
 }
 
-const abreModalPostagem = () => {
+let modal_status_postagem = document.getElementById('modal_status_postagem');
 
+const abreModalPostagem = () => {
+  modal_status_postagem.classList.remove('hidden'); 
+  modal_status_postagem.classList.add('flex');
 }
 
 const fechaModalPostagem = () => {
-  
+  modal_status_postagem.classList.add('hidden');
 }
