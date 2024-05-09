4b01e168677f8d4c32604ddb09570d9a358610a8
Rafael Belchior <rafaelb89.rb@gmail.com>
2024-05-09 10:56:27 -0300

    update form response

--- resources/views/layouts/maps/index.blade.php
+++ resources/views/layouts/maps/index.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <div class="sm:w-5/6 w-full ml-auto lg:px-12 p-4 flex justify-center">
         <div class="sm:w-3/4 w-full bg-white shadow rounded p-4">
-            <div class="flex items-center text-gray-500 font-bold text-4xl">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+            <div class="flex items-center text-gray-500 font-bold text-2xl sm:text-4xl">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 sm:w-10 sm:h-10">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 0 0-8.862 12.872M12.75 3.031a9 9 0 0 1 6.69 14.036m0 0-.177-.529A2.25 2.25 0 0 0 17.128 15H16.5l-.324-.324a1.453 1.453 0 0 0-2.328.377l-.036.073a1.586 1.586 0 0 1-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 0 1-5.276 3.67m0 0a9 9 0 0 1-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
                 </svg>
                 <h1 class="ml-1">Mapa de Usuários</h1>
             </div>
             <form action="{{route('maps')}}" method="GET" class="border p-4 rounded mt-6">
-                <div class="flex space-x-6">
+                <div class="flex sm:flex-row flex-col space-x-0 sm:space-x-6">
                     <div class="flex flex-col w-full mt-4">
                         <label for="date_inicio" class="text-sm text-gray-700">A partir da data</label>
                         <input type="text" id="date_inicio" name="date_inicio" placeholder="dd/mm/yyyy" class="cliente px-1 py-2 w-full border outline-none rounded bg-white border-gray-200 text-sm text-gray-600">
@@ -29,7 +29,7 @@
                     </div>
                 </div>
 
-                <div class="flex space-x-6">
+                <div class="flex sm:flex-row flex-col space-x-0 sm:space-x-6">
                     <div class="flex flex-col w-full mt-4">
                         <label for="view_agencia_correios" class="text-sm text-gray-700">Deseja Visualizar as Agencias dos Correios?</label>
                         <select id="view_agencia_correios" name="view_agencia_correios" class="px-1 py-2 w-full border outline-none rounded bg-white border-gray-200 text-sm text-gray-600">
